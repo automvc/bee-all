f937b383a7af045bd309ea410fe39d265f801284
Kingstar <honeysoft@126.com>
2023-05-14 23:52:18 +0800

    bee-all 2.1

---
+++ src/main/java/org/teasoft/bee/all/BeeAllVersion.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2016-2023 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.bee.all;
+
+import org.teasoft.bee.osql.BeeVersion;
+
+/**
+ * Bee-all include project Bee,Honey,Bee-Ext.
+ * Bee-all's version same as Bee.
+ * @author Kingstar
+ * @since  2.1
+ */
+public class BeeAllVersion {
+
+	public static final String version = BeeVersion.version;
+	public static final String buildId = BeeVersion.buildId;
+
+	private BeeAllVersion() {}
+
+}
